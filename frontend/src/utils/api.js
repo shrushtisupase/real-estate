3a3638468a67711da563d8d893935fda7f480982
Shrushti Supase <supase.shrushti@gmail.com>
2026-07-12 01:08:52 +0530

    --

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // No baseURL specified so it goes to Vite proxy (which redirects to http://127.0.0.1:5000)
+  baseURL: import.meta.env.VITE_API_BASE_URL || '',
   headers: {
     'Content-Type': 'application/json',
   },
